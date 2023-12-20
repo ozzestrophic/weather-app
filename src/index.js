077f69a53ce21b491d94c0e13a072aa7c2d40b08
Ozzeek <osama.m.alnagar@gmail.com>
2023-12-21 00:02:42 +0200

    perf: wrapped fetch and fill in wrapper function

--- src/index.js
+++ src/index.js
@@ -11,15 +11,6 @@ async function getWeather(place) {
   }
 }
 
-const submitBtn = document.getElementById("submit");
-
-submitBtn.addEventListener("click", async function (event) {
-  event.preventDefault();
-  const place = submitBtn.previousElementSibling.value;
-  const data = await getWeather(place);
-  fillData(data);
-});
-
 function fillData(data) {
   document.getElementById("condition").innerText = data.current.condition.text;
   document.getElementById("city").innerText = data.location.name;
@@ -30,11 +21,37 @@ function fillData(data) {
   document.getElementById("windSpeed").innerText = data.current.wind_kph;
 }
 
+async function fetchAndFill(place) {
+  const data = await getWeather(place);
+  fillData(data);
+}
+
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener("click", async function (event) {
+  event.preventDefault();
+  const place = submitBtn.previousElementSibling.value;
+  fetchAndFill(place);
+});
+
+// toggle stupid units
 const toggleUnit = document.getElementById("toggleUnit");
-toggleUnit.addEventListener("click", function (event) {
-  if (event.target.innerText === "Stupid units") {
-    event.target.innerText = "Smart units";
+toggleUnit.addEventListener("click", async function (event) {
+  if (event.target.innerText === "🤡 Stupid units") {
+    const data = await getWeather(document.getElementById("input").value);
+    document.getElementById("degree").innerText = data.current.temp_f;
+    document.getElementById("degreeUnit").innerText = "°F";
+    document.getElementById("windSpeed").innerText = data.current.wind_mph;
+    document.getElementById("windUnit").innerText = "MpH";
+    event.target.innerText = "👨🏻‍🔬 Smart units";
   } else {
-    event.target.innerText = "Stupid units";
+    const data = await getWeather(document.getElementById("input").value);
+    document.getElementById("degree").innerText = data.current.temp_c;
+    document.getElementById("degreeUnit").innerText = "°C";
+    document.getElementById("windSpeed").innerText = data.current.wind_kph;
+    document.getElementById("windUnit").innerText = "KpH";
+    event.target.innerText = "🤡 Stupid units";
   }
 });
+
+fetchAndFill("kuala lumpur");
